a4d1f66bc2e851dab5ceebec076706c67db5a042
mintkat <lucasbear9@gmail.com>
2024-12-22 10:10:16 -0900

    Update haxelib.json classPath and enhance App class with rootNode functionality

--- src_api/lucidKit/App.hx
+++ src_api/lucidKit/App.hx
@@ -1,6 +1,14 @@
 package lucidKit;
 
+import lucidKit.godot.Node;
+
 class App {
+    public var rootNode(get, default) : Node;
+
+    public function get_rootNode() : Node {
+        return untyped __lua__("rootNode");
+    }
+
     public function new() {
         untyped __lua__("_G.process = function(delta) self:process(delta) end");
         untyped __lua__("_G.physicsProcess = function(delta) self:physicsProcess(delta) end");
