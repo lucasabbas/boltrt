8254e8b538d2af304501ebedd778305a6df4b4da
mintkat <lucasbear9@gmail.com>
2025-01-03 12:27:13 -0900

    Fixes for linux

--- src/Main.cs
+++ src/Main.cs
@@ -4,35 +4,35 @@ using System;
 
 public class Main : Node
 {
-    LuaNode _luaNode;
-    
-    public override void _Ready()
-    {
-        bool editorMode = false;
+	LuaNode _luaNode;
+	
+	public override void _Ready()
+	{
+		bool editorMode = false;
 
-        var args = OS.GetCmdlineArgs();
-        for (int i = 0; i < args.Length; i++)
-        {
-            if (args[i] == "--dev")
-            {
-                editorMode = true;
-                break;
-            }
-        }
+		var args = OS.GetCmdlineArgs();
+		for (int i = 0; i < args.Length; i++)
+		{
+			if (args[i] == "--dev")
+			{
+				editorMode = true;
+				break;
+			}
+		}
 
-        if (OS.GetName() == "HTML5")
-        {
-            GetTree().ChangeScene("res://src/WebPlayer.tscn");
-            return;
-        }
+		if (OS.GetName() == "HTML5")
+		{
+			GetTree().ChangeScene("res://src/WebPlayer.tscn");
+			return;
+		}
 
-        if (editorMode)
-        {
-            GetTree().ChangeScene("res://src/Editor.tscn");
-        }
-        else
-        {
-            GetTree().ChangeScene("res://src/Standalone.tscn");
-        }
-    }
+		if (editorMode)
+		{
+			GetTree().ChangeScene("res://src/Editor.tscn");
+		}
+		else
+		{
+			GetTree().ChangeScene("res://src/Standalone.tscn");
+		}
+	}
 }
